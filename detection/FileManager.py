e814b20d87956922822d38ff01764f905baf3f03
bkrumnow <benjamin.krumnow@th-koeln.de>
2020-02-16 19:19:11 +0100

    Update userAgent

--- detection/FileManager.py
+++ detection/FileManager.py
@@ -29,17 +29,19 @@ with open(os.path.join('detection/configuration','config.json')) as json_data_fi
 def downloadFile(url):
         data = ''
         # TODO: Needs to be switched with a recent user agent each start
-        http_header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-               'Accept-Encoding': 'none',
-               'Accept-Language': 'en-US,en;q=0.8',
-               'Connection': 'keep-alive'}
+        http_header = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko)\
+                Chrome/23.0.1271.64 Safari/537.11',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                'Accept-Encoding': 'none',
+                'Accept-Language': 'en-US,en;q=0.8',
+                'Connection': 'keep-alive'}
 
         # A src attribute may not contain a http(s) prefix
         if url.startswith('//'):
             url = 'http:' + url
-               
+
         try:
             r = requests.get(url, headers=http_header)
         except requests.exceptions.RequestException as e:
